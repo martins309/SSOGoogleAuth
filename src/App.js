0e5df99dcb67ba7665adcf5551ec38d39e160f36
Andrew Martins <andrew_omartins@yahoo.com>
2022-06-20 20:49:27 -0400

    added full google auth functionality with welcome message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import jwt_decode from "jwt-decode"
 
 function App() {
+  const [ user, setUser ] = useState({})
 
   function handleCallBackResponse(response) {
     console.log("Encoded JWT ID Token:" + response.credential)
     const userObject = jwt_decode(response.credential)
     console.log(userObject)
+    setUser(userObject)
+    document.getElementById("signInDiv").hidden = true
+  }
+
+  function handleSignOut(event){
+    setUser({})
+    document.getElementById("signInDiv").hidden = false
   }
 
   useEffect(() => {
@@ -26,10 +33,18 @@ function App() {
 
   },[])
 
-
+//if there is no user then we want to show the sign in button if not we want the sign out
   return (
     <div className="App">
       <div id="signInDiv"></div>
+      {
+        Object.keys(user).length != 0 &&
+      <button onClick= { (e) => handleSignOut(e)}>Sign Out</button> 
+      && 
+        <h3> Welcome {user.name}, Nice to see you</h3>
+      
+      }
+      
     </div>
   );
 }
